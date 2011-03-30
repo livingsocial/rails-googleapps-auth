c3e14ed3b89ff972fef062939bb7a3a0be19ec8d
Rob Ares <rob.ares@gmail.com>
2011-03-30 11:19:34 -0400

    DOH! use the right key for version

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'googleapps_auth'
 
-config.gem "ruby-openid", :lib => "2.1.8"
+config.gem "ruby-openid", :version => "2.1.8"
 
 config.after_initialize do
   ActionController::Base.send :include, GoogleAppsAuth
