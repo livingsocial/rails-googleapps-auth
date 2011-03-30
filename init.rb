ffd59bb018d8a8df72988de7dff65ac195903db1
Rob Ares <rob.ares@gmail.com>
2011-03-30 11:48:58 -0400

    add gem assertion before the require

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
-require 'googleapps_auth'
-
 config.gem "ruby-openid", :version => "2.1.8", :lib => "openid"
+
+require 'googleapps_auth'
 
 config.after_initialize do
   ActionController::Base.send :include, GoogleAppsAuth
